98ac8d6a53f861c548177d930c9e60c9a0422bad
Thomas <tschuba@gmail.com>
2025-10-02 18:24:27 +0200

    Add DataSource configuration and SQLite dependencies

---
+++ server/src/main/java/tschuba/ez/booth/ServerApplication.java
@@ -0,0 +1,11 @@
+package tschuba.ez.booth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+    static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
