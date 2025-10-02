537f851ff9b51d753ff72025618aefbd3be35268
Thomas <tschuba@gmail.com>
2025-10-02 23:45:22 +0200

    Add JPA entities for booths, vendors, purchases, and purchase items

---
+++ core/src/main/java/tschuba/ez/booth/Entities.java
@@ -0,0 +1,176 @@
+package tschuba.ez.booth;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * JPA entities for {@link DataModel}.
+ */
+public class Entities {
+
+    private static final String SCHEMA = "ez_booth";
+
+    private Entities() {
+    }
+
+    @Entity
+    @Table(name = "booths", schema = SCHEMA)
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    public static class Booth {
+
+        @EmbeddedId
+        @EqualsAndHashCode.Include
+        private Key key;
+
+        @Column(nullable = false, name = "description")
+        private String description;
+
+        @Column(nullable = false, name = "date")
+        private LocalDate date;
+
+        @Column(nullable = false, name = "participation_fee")
+        private BigDecimal participationFee;
+
+        @Column(nullable = false, name = "sales_fee")
+        private BigDecimal salesFee;
+
+        @Column(nullable = false, name = "fees_rounding_step")
+        private BigDecimal feesRoundingStep;
+
+        @Column(name = "closed")
+        private boolean closed;
+
+        @Column(name = "closed_on")
+        private LocalDateTime closedOn;
+
+        @Embeddable
+        @Builder
+        @NoArgsConstructor
+        @AllArgsConstructor
+        @Getter
+        @EqualsAndHashCode
+        public static class Key {
+            @Column(nullable = false, name = "booth_id")
+            private String boothId;
+        }
+    }
+
+    @Entity
+    @Table(name = "vendors", schema = SCHEMA)
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+    public static class Vendor {
+
+        @EmbeddedId
+        @EqualsAndHashCode.Include
+        private Key key;
+
+        @Embeddable
+        @Builder
+        @NoArgsConstructor
+        @AllArgsConstructor
+        @Getter
+        @EqualsAndHashCode
+        public static class Key {
+            @Embedded
+            private Booth.Key booth;
+
+            @Column(nullable = false, name = "vendor_id")
+            private String vendorId;
+        }
+    }
+
+    @Entity
+    @Table(name = "purchases", schema = SCHEMA)
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+    public static class Purchase {
+
+        @EmbeddedId
+        @EqualsAndHashCode.Include
+        private Purchase.Key key;
+
+        @Column(nullable = false, name = "value")
+        private BigDecimal value;
+
+        @Column(nullable = false, name = "purchased_on")
+        private LocalDateTime purchasedOn;
+
+        @OneToMany(mappedBy = "purchase", cascade = CascadeType.ALL, orphanRemoval = true)
+        private List<PurchaseItem> items = new ArrayList<>();
+
+        @Embeddable
+        @Builder
+        @NoArgsConstructor
+        @AllArgsConstructor
+        @Getter
+        @EqualsAndHashCode
+        public static class Key {
+            @Column(nullable = false, name = "booth")
+            private Booth.Key booth;
+
+            @Column(nullable = false, name = "purchase_id")
+            private String purchaseId;
+        }
+
+    }
+
+    @Entity
+    @Table(name = "purchase_items", schema = SCHEMA)
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+    public static class PurchaseItem {
+
+        @EmbeddedId
+        @EqualsAndHashCode.Include
+        private Key key;
+
+        @ManyToOne
+        @JoinColumns({
+                @JoinColumn(name = "booth", referencedColumnName = "booth"),
+                @JoinColumn(name = "purchase_id", referencedColumnName = "purchase_id")
+        })
+        private Purchase purchase;
+
+        @Embedded
+        private Vendor.Key vendor;
+
+        @Column(nullable = false, name = "price")
+        private BigDecimal price;
+
+        @Column(nullable = false, name = "purchased_on")
+        private LocalDateTime purchasedOn;
+
+        @Embeddable
+        @Builder
+        @NoArgsConstructor
+        @AllArgsConstructor
+        @Getter
+        @EqualsAndHashCode
+        public static class Key {
+            @Embedded
+            private Purchase.Key purchase;
+
+            @Column(nullable = false, name = "item_id")
+            private String itemId;
+        }
+    }
+}
