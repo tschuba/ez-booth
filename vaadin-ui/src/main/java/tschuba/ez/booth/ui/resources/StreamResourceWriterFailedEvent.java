86e16f4f4823c79655d99fa67e959169aa6e5f05
Thomas <tschuba@gmail.com>
2025-10-09 15:11:21 +0200

    Remove obsolete resource classes

--- vaadin-ui/src/main/java/tschuba/ez/booth/ui/resources/StreamResourceWriterFailedEvent.java
+++
@@ -1,7 +0,0 @@
-/**
- * Copyright (c) 2025 Thomas Schulte-Bahrenberg
- * All rights reserved.
- */
-package tschuba.ez.booth.ui.resources;
-
-public record StreamResourceWriterFailedEvent(Exception error) {}
